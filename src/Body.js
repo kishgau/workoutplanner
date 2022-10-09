5489c619bc0568ac6b44116290967cade7b0a08c
Gaurav Sharma <Gaurav.Sharma@versent.com.au>
2022-10-09 21:42:04 +1000

    License and data.json

--- src/Body.js
+++ src/Body.js
@@ -3,38 +3,31 @@ const Body = () => {
  <div className="container">
    <div className="card">
       <h2>Monday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
    <div className="card">
       <h2>Tuesday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
    <div className="card">
       <h2>Wednesday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
    <div className="card">
       <h2>Thursday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
    <div className="card">
       <h2>Friday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
    <div className="card">
       <h2>Saturday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
    <div className="card">
       <h2>Sunday</h2>
-      <h2>Title</h2>
-      <h2>Difficulty</h2>
+      <h2>Excercise List</h2>
    </div>
  </div>
 
